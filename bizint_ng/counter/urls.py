ac817030b3e3e50bb081857a10bb34be8459f3b5
Benji <benjitobias@gmail.com>
2020-10-29 16:17:47 +0200

    Added basic pie chart

--- bizint_ng/counter/urls.py
+++ bizint_ng/counter/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:action_id>/', views.info, name='info'),
     path('<int:action_id>/add', views.add, name='add'),
+    #TODO: remove from here, will be in info
+    path('populate-actions-chart/', views.populate_actions_chart, name='populate_actions_chart'),
 ]
--- bizint_ng/counter/views.py
+++ bizint_ng/counter/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from guardian.shortcuts import get_objects_for_user
 from guardian.decorators import permission_required_or_403
+from django.db.models import Count as CountFunction
+from django.http import JsonResponse
+from guardian.shortcuts import get_objects_for_user
 
-from .models import Action
+from .models import Action, Count
 
 
 class IndexView(generic.ListView):
@@ -31,3 +34,21 @@ def add(request, action_id):
     action.count_set.create(count=action.get_count() + 1)
 
     return render(request, 'counter/info.html', {'action': action})
+
+
+#TODO: implement into info
+def populate_actions_chart(request):
+    labels = []
+    data = []
+    if request.user.is_authenticated:
+        actions = get_objects_for_user(request.user, 'counter.view_action')
+
+        for action in actions:
+            labels.append(action.name)
+            data.append(action.get_count())
+
+        data = {
+            'labels': labels,
+            'data': data
+        }
+    return JsonResponse(data=data)
