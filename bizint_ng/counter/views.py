bf61140c069425e639c998a7d683476171a5f8ca
Benji <benjitobias@gmail.com>
2020-10-29 02:59:31 +0200

    cleaned up

--- bizint_ng/counter/views.py
+++ bizint_ng/counter/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from django.views import generic
 from guardian.shortcuts import get_objects_for_user
 from guardian.decorators import permission_required_or_403
@@ -25,12 +22,12 @@ def index(request):
 @permission_required_or_403('counter.view_action', (Action, 'id', 'action_id'))
 def info(request, action_id):
     action = get_object_or_404(Action, pk=action_id)
-    count = action.get_count()
-    return render(request, 'counter/info.html', {'action': action, 'count': count})
+    return render(request, 'counter/info.html', {'action': action})
 
 
 @permission_required_or_403('counter.change_action', (Action, 'id', 'action_id'))
 def add(request, action_id):
     action = get_object_or_404(Action, pk=action_id)
     action.count_set.create(count=action.get_count() + 1)
-    return HttpResponse(action.get_count())
+
+    return render(request, 'counter/info.html', {'action': action})
