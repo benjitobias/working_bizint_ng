5bf26ae6cf218969b7ae288e1ed6de98a7a99630
Benji <benjitobias@gmail.com>
2021-05-25 01:54:52 +0300

    long/lat added to form

--- bizint_ng/counter/forms.py
+++ bizint_ng/counter/forms.py
@@ -4,5 +4,6 @@ from django import forms
 class CountForm(forms.Form):
     note = forms.CharField(label='Note', max_length=200, required=False)
     update_telegram = forms.BooleanField(label='Update Telegram?', required=False)
-
-
+    longitude = forms.FloatField(label='longitude', widget=forms.HiddenInput(), initial=0)
+    latitude = forms.FloatField(label='latitude', widget=forms.HiddenInput(), initial=0)
+    ignore_location = forms.BooleanField(label='Ignore location?', required=False, widget=forms.CheckboxInput(attrs={'onclick': 'updatePosition();'}))
